6c0bd18449155606364a50a4a6f5d034fc95daeb
Oscar Jiménez <57319335+ojimenezjh@users.noreply.github.com>
2022-06-18 02:17:40 +0200

    Delete original client id from variables.js

--- src/js/variables.js
+++ src/js/variables.js
@@ -1,2 +1,2 @@
 // Google OAuth2 client id
-var clientId = '1047584073634-opr4tf1fednhjvu7js7l6s85akjb3r0b.apps.googleusercontent.com'
+var clientId = 'YOUR_OAUTH2_CLIENT_ID.apps.googleusercontent.com'
