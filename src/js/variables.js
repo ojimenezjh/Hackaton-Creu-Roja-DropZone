a8c12667277900465a5bd036102f8cfb60d5d3b6
ojimenezjh <ojimenezjh@uoc.edu>
2022-06-25 16:38:09 +0200

    push definitivo

--- src/js/variables.js
+++ src/js/variables.js
@@ -1,6 +1,2 @@
 // Google OAuth2 client id
-<<<<<<< HEAD
 var clientId = '1047584073634-opr4tf1fednhjvu7js7l6s85akjb3r0b.apps.googleusercontent.com'
-=======
-var clientId = 'YOUR_CLIENT_ID.apps.googleusercontent.com'
->>>>>>> afac67440eccc96770ec86d11b0a047ef95425b7
